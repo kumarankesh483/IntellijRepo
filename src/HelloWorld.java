6a04d963021232621e517d30bcb90fcfba3e4eb9
kumarankesh483 <kumarankesh483@gmail.com>
2022-05-20 22:20:53 +0530

    this is second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("this is new world of intellij, hello world");
+        System.out.println("this is new commit");
     }
 }
